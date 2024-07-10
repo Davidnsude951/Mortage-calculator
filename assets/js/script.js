7f73a95a461f6632ce1f27acd007ca648ceb7c7b
Davidnsude951 <davidnsude951@gmail.com>
2024-07-10 10:04:54 +0100

    .css is done

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+const calculate = document.getElementById(`calculate`)
+const initial = document.querySelector(`.initial`)
+const final = document.querySelector(`.final`)
+
+// const initial = document.querySelector(`.initial`)
+// const final = document.querySelector(`.final`)
+
+// if(final.style.display ===`none`){
+//       final.style.display=`flex`
+//       initial.style.display=`none`
+// }
+// else{
+//       final.style.display=`none`
+//       initial.style.display=`flex`
+// }
+
+
+calculate.addEventListener(`click`,()=>{
+      
+})
